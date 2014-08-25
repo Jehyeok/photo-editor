9ece2a6f2518f2d6031cc5ff28e239382718d464
Jehyeok <hook3748@gmail.com>
2014-08-25 16:21:42 +0900

    중복 제거 및 mo move 구현

--- webapp/javascripts/ContentManager.js
+++ webapp/javascripts/ContentManager.js
@@ -9,8 +9,7 @@ var ContentUploader = {
 			
 			this.reader.readAsDataURL(this.fileInput.files[0]);
 			this.reader.onloadend = function() {
-				var imageInSidebarTemplate = _.template(document
-						.getElementById('imageInSidebarTemplate').innerHTML);
+				var imageInSidebarTemplate = _.template(document.getElementById('imageInSidebarTemplate').innerHTML);
 				var image = imageInSidebarTemplate({
 					backgroundURL : this.reader.result
 				});
@@ -26,58 +25,51 @@ var ContentUploader = {
 var ContentMover = {
 		content: document.getElementById('content'),
 		rightSidebar: document.getElementById('rightSidebarWrapper'),
+		selectedImage: null,
 		
-		init: function() {
-			this.rightSidebar.addEventListener('dragstart', function(e) {
-				console.log('dragstart');
+		dragStart: function(e) {
+			console.log('dragstart');
 
-				var selectedDiv = e.target;
-				if (e.target.tagName === 'DIV') {
-					var imageURL = selectedDiv.style['background-image'];
-					var URLComponent = imageURL.slice(4, imageURL.length - 1);
-					
-					e.dataTransfer.setData('imageURL', imageURL);
-					e.dataTransfer.setData('initOffsetX', e.offsetX);
-					e.dataTransfer.setData('initOffsetY', e.offsetY);
-				}
-			}.bind(this), false);
-			
-			this.content.addEventListener('dragstart', function(e) {
-				console.log('dragstart');
+			this.selectedImage = e.target;
+			if (e.target.tagName === 'DIV') {
+				var imageURL = this.selectedImage.style['background-image'];
+				var URLComponent = imageURL.slice(4, imageURL.length - 1);
 				
-				var selectedDiv = e.target;
-				if (e.target.tagName === 'DIV') {
-					var imageURL = selectedDiv.style['background-image'];
-					var URLComponent = imageURL.slice(4, imageURL.length - 1);
-					
-					e.dataTransfer.setData('imageURL', imageURL);
-					e.dataTransfer.setData('initOffsetX', e.offsetX);
-					e.dataTransfer.setData('initOffsetY', e.offsetY);
-				}
-			}.bind(this), false);
-
+				e.dataTransfer.setData('imageURL', imageURL);
+				e.dataTransfer.setData('offsetXInImageBox', e.offsetX);
+				e.dataTransfer.setData('offsetYInImageBox', e.offsetY);
+				e.dataTransfer.setData('isMove', e.currentTarget.className === 'content');
+			}
+		},
+		init: function() {
+			this.rightSidebar.addEventListener('dragstart', this.dragStart.bind(this), false);
+			this.content.addEventListener('dragstart', this.dragStart.bind(this), false);
 			this.content.addEventListener('dragover', function(e) {
 				e.preventDefault();
 			}, false);
-
 			this.content.addEventListener('drop', function(e) {
 				if (e.target.className === 'content') {
 					var imageURL = e.dataTransfer.getData('imageURL');
-					var left = e.offsetX - e.dataTransfer.getData('initOffsetX') + 'px';
-					var top = e.offsetY - e.dataTransfer.getData('initOffsetY') + 'px';
-
-					var imageInContentTemplate = _.template(document
-							.getElementById('imageInContentTemplate').innerHTML);
-					var image = imageInContentTemplate({
-						backgroundURL : imageURL,
-						width : '180px',
-						height : '180px',
-						left : left,
-						top : top
-					});
-					this.insertAdjacentHTML('beforeend', image);
+					var left = e.offsetX - e.dataTransfer.getData('offsetXInImageBox') + 'px';
+					var top = e.offsetY - e.dataTransfer.getData('offsetYInImageBox') + 'px';
+					var isMove = ('true' === e.dataTransfer.getData('isMove'));
+					
+					if (isMove) {
+						this.selectedImage.style.left = left;
+						this.selectedImage.style.top = top;
+					} else{
+						var imageInContentTemplate = _.template(document.getElementById('imageInContentTemplate').innerHTML);
+						var image = imageInContentTemplate({
+							backgroundURL : imageURL,
+							width : '180px',
+							height : '180px',
+							left : left,
+							top : top
+						});
+						this.content.insertAdjacentHTML('beforeend', image);
+					}
 				}
-			}, false);
+			}.bind(this), false);
 		}
 };
 
